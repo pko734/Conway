4ff6551d107d8ff2a96dfa034649a17a816c0216
Paul Oehler <poehler@interworx.com>
2018-07-19 23:23:38 -0400

    Assignment 4: checkpoint 1

--- Assignment4/Assignment4/InstrumentationViewController.swift
+++ Assignment4/Assignment4/InstrumentationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  Assignment4
 //
 //  Created by Van Simmons on 6/5/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class InstrumentationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
